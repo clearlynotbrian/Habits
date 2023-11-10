1d43bc417dea4670b7f810288b23ab2b4dd81d6b
clearlynotbrian <kietd311@gmail.com>
2023-11-10 15:36:17 -0800

    Added Habit +-, cleaned up create habit/daily
    Changed habit checkbox to a + and a -. In addition, adding a habit/daily actually create a task in their own respective tables. Now there are two js functions, one for habit and one for daily.

--- pages/main.js
+++ pages/main.js
@@ -1,8 +1,26 @@
 
+function addHabit(ele) {
+    if(event.key === "Enter") {
+        console.log("pressed");
+        document.getElementById("habit").innerHTML += `
+            <div class="daily-task">
+                <div class="task-inc">
+                    <button class = "task-increment" type = "button"> + </button>
+                </div>
+                <div class="task-description">
+                    <p>` + ele.value + `</p>
+                </div>
+                <div class="task-dec">
+                    <button class = "task-decrement" type = "button"> - </button>
+                </div>
+            </div>`;
+
+    }
+};
 function addTask(ele) {
     if(event.key === 'Enter') {
-        console.log("enter is pressed")
-        document.getElementById("habits").innerHTML += `
+        console.log("enter is pressed");
+        document.getElementById("daily").innerHTML += `
         <div class="daily-task">
             <div class="task-check-box">
             <input type="checkbox" name="myCheckbox" id="myCheckBox">
@@ -11,8 +29,6 @@ function addTask(ele) {
             <div class="task-description">
                 <p>` + ele.value + `</p>
             </div>
-        </div>`;
-
-        
+        </div>`;        
     }
 }
